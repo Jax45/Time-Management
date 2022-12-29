80d545d3e1d7bef80200643a4537f3ed3da043ea
Jackson T. Hoenig <buzz@hoenig.net>
2022-12-29 07:23:34 -0600

    Initial commit

---
+++ app/src/main/java/superbyte/software/timemanagment/calendar/CalendarFragment.kt
@@ -0,0 +1,45 @@
+package superbyte.software.timemanagment.calendar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import superbyte.software.timemanagment.R
+import superbyte.software.timemanagment.databinding.FragmentCalendarBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class CalendarFragment : Fragment() {
+
+    private var _binding: FragmentCalendarBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
